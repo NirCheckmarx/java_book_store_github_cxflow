fdbe4833a1842884a921f8126e253667d2461e6b
Nir Weinberg <Nir.Weinberg@checkmarx.com>
2021-12-20 21:17:26 +1100

    add git commit comment script

---
+++ CxComment.groovy
@@ -0,0 +1,12 @@
+@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7' )
+import groovyx.net.http.HTTPBuilder
+
+def branch = request.getBranch()
+def env = System.getenv()
+def commitId = env['GITHUB_SHA']
+def repoUrl = env['GITHUB_REPOSITORY']
+String scanComment = "Repo: $repoUrl | Branch: $branch | Commit ID: $commitId"
+
+println "INFO : Scanning code from $scanComment"
+
+return scanComment
